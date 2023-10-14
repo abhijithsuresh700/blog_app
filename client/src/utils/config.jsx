594718832e09b297194c075552d9d2d69e36379d
Abhijith Suresh <abhijithsuresh700@gmail.com>
2023-10-14 17:24:26 +0530

    render server added

--- client/src/utils/config.jsx
+++ client/src/utils/config.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
-//   baseURL: "https://nile-booking.onrender.com/api"
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://blog-app-niyl.onrender.com/api"
 });
